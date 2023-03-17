bb3a335c236e4da1ed5a31af4b5939e85eb82862
Furkan-Gulsen <m.furkangulsen@hotmail.com>
2023-03-17 03:11:55 +0300

    generate product service

---
+++ product_service/lib/product_service-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ProductServiceStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+		// * api gateway
+	}
+}
